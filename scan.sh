7e16737c05ac10310f13e4d12e38918b725016e0
Gergely Imreh <imrehg@gmail.com>
2017-09-01 11:40:55 +0100

    add more info to the test output: local BT device and found devices
    Change-type: patch

--- scan.sh
+++ scan.sh
@@ -4,10 +4,17 @@ FAILED=0
 
 echo "Testing bluetooth. Make sure you have a bluetooth device enabled and visible."
 
-echo "Scan for devices..."
-if [ `hcitool scan | wc -l` -le 1 ]; then
+hcitool dev
+
+echo "Scan for nearby blueooth devices..."
+mapfile -t scanresult < <(hcitool scan)
+if [ ${#scanresult[@]} -le 1 ]; then
     FAILED=1
 else
+    echo "Found these devices:"
+    for (( i=1; i<${#scanresult[@]}; i++ )); do
+        echo ${scanresult[i]};
+    done
     FAILED=0
 fi
 
